1e0a3f4785c77424e3f6a837e65afc1c4ce8bf47
Oleg <filinskyi@gmail.com>
2019-11-20 02:05:52 +0200

    Created Calendar component, created database in localstorage

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import * as myJson from '@/assets/events'
 
 Vue.config.productionTip = false
 
+let eventsArray = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : myJson.events
+localStorage.setItem('events', JSON.stringify(eventsArray))
+const data = JSON.parse(localStorage.getItem('events'))
+
+export const db = data
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
